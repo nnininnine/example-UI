8ad5965e431d972089411be8b3e133a657293fe9
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-29 12:06:22 +0700

    feat(main): implement binding table view

--- example-UI/Screens/Main/View/MainViewController.swift
+++ example-UI/Screens/Main/View/MainViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
 import UIKit
 
 class MainViewController: UITableViewController {
+    // MARK: Properties
+
+    lazy var viewModel: MainViewModel = .init()
+
     // MARK: Init
 
     override func viewDidLoad() {
@@ -19,5 +25,23 @@ class MainViewController: UITableViewController {
 
     // MARK: Methods
 
-    func setup() {}
+    func setup() {
+        // binding
+        bindingTableView()
+    }
+
+    func bindingTableView() {
+        tableView.delegate = nil
+        tableView.dataSource = nil
+
+        viewModel.modules.bind(to: tableView.rx.items) { (_: UITableView, _: Int, module: Module) in
+            let cell = TableViewCell(style: .default, reuseIdentifier: "cell")
+            cell.textLabel?.text = module.name
+            return cell
+        }.disposed(by: viewModel.disposeBag)
+    }
+}
+
+private class TableViewCell: UITableViewCell {
+    override func setSelected(_ selected: Bool, animated: Bool) {}
 }
