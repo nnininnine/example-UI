b624e732aec2f703f493befb5f4aa68bee28baec
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-29 15:07:47 +0700

    feat: add font

---
+++ example-UI/Screens/DiaryFolder/View/DiaryFolderViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryFolderViewController.swift
+//  example-UI
+//
+//  Created by Nuttapon Buaban on 29/6/2565 BE.
+//
+
+import Foundation
+import UIKit
+
+class DiaryFolderViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
