edd5876515b16033c1167e272014b5d7a7c2f882
Nuttapon Buaban <nuttapon.dev@gmail.com>
2022-06-29 11:09:43 +0700

    feat(main): create MainViewModel class

---
+++ example-UI/Screens/Main/ViewModel/MainViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewModel.swift
+//  example-UI
+//
+//  Created by Nuttapon Buaban on 29/6/2565 BE.
+//
+
+import Foundation
+
+class MainViewModel {
+    // MARK: Properties
+
+    // MARK: Init
+
+    init() {}
+
+    // MARK: Methods
+}
